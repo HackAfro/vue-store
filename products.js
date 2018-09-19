81884d47c65933072355626f9f9a8d481b0a7daa
Richard Afro <richyafro@hotmail.com>
2018-09-19 14:30:04 +0100

    Add products

---
+++ products.js
@@ -0,0 +1,464 @@
+module.exports = [
+  {
+    title: 'Non coating waist jacket',
+    description:
+      'The best coat and wait jacket money can buy. Hand made with long lasting fibre',
+    image: 'v1530025449/samples/bike.jpg',
+    discountPrice: '$12',
+    previousPrice: '$16',
+    badge: 'new',
+    category: ['women', 'new'],
+  },
+  {
+    title: 'Best jogging shoes',
+    description:
+      'This is the best jogging shoes known to man. Feels sweet on your foot',
+    image: 'v1530025448/samples/ecommerce/shoes.png',
+    discountPrice: '$60',
+    previousPrice: '',
+    badge: 'new',
+    category: 'new',
+  },
+  {
+    title: 'Old school leather bag',
+    description:
+      'The best coat and wait jacket money can buy. Hand made with long lasting fibre',
+    image: 'v1530025457/samples/ecommerce/leather-bag-gray.jpg',
+    discountPrice: '$240',
+    previousPrice: '$320',
+    badge: 'sale',
+    category: ['men', 'sale'],
+  },
+  {
+    title: 'Blue winter jacket',
+    description: 'Keeps the cold away, lets the kid enjoy the snow',
+    image: 'v1530025450/samples/people/boy-snow-hoodie.jpg',
+    discountPrice: '$60',
+    previousPrice: '',
+    badge: '',
+    category: ['men'],
+  },
+  {
+    title: 'Exotic print t-shirt',
+    description:
+      "Exotic long lasting T-shirt. Lasts longer than you'll expect. No lies spoken",
+    image: 'v1531899989/samples/attendee_avatar.jpg',
+    discountPrice: '$16',
+    previousPrice: '',
+    badge: '',
+    category: ['men'],
+  },
+  {
+    title: 'The assorted collection',
+    description:
+      'A collection of our best products yet. At a highly discounted price.',
+    image: 'v1530025458/samples/ecommerce/accessories-bag.jpg',
+    discountPrice: '$300',
+    previousPrice: '$415',
+    badge: 'sale',
+    category: ['sale', 'men'],
+  },
+  {
+    title: 'Soft blue t-shirt',
+    description:
+      'Plain blue t-shirt that can be worn underneath your everyday jacket. Switch it up',
+    image: 'v1530025447/samples/people/smiling-man.jpg',
+    discountPrice: '$5',
+    previousPrice: '$16',
+    badge: 'new',
+    category: ['new', 'men'],
+  },
+  {
+    title: 'Beautiful self cleaning chef jacket',
+    description:
+      "What's better than a self cleaning jacket. Don't beat yourself up for spilling food on it. It knows you're clumsy",
+    image: 'v1530025442/samples/people/kitchen-bar.jpg',
+    discountPrice: '$46',
+    previousPrice: '',
+    badge: '',
+    category: ['men'],
+  },
+  {
+    title: 'Brown leather wrist watch',
+    description:
+      'The kind you hand over to your grandkids when they start being nice to you',
+    image: 'v1530025440/samples/ecommerce/analog-classic.jpg',
+    discountPrice: '$120',
+    previousPrice: '$156',
+    badge: 'sale',
+    category: ['sale', 'men'],
+  },
+  {
+    title: 'Comfortable flannel shrit',
+    description:
+      'Real comfy flannel shirt with high quality cotton. Available in a set of bright colors',
+    image: 'v1537361999/samples/flannel-shirt.jpg',
+    discountPrice: '$120',
+    previousPrice: '',
+    badge: 'new',
+    category: ['new', 'men'],
+  },
+  {
+    title: 'Non coating waist jacket',
+    description:
+      'The best coat and wait jacket money can buy. Hand made with long lasting fibre',
+    image: 'v1537361998/samples/man-on-balcony.jpg',
+    discountPrice: '$120',
+    previousPrice: '$210',
+    badge: 'sale',
+    category: ['sale', 'men'],
+  },
+  {
+    title: 'Grey thick hoodie',
+    description:
+      'Attack the cold with this comfy hoodie that keeps you warm during that cold evening run',
+    image: 'v1537361998/samples/hoodie.jpg',
+    discountPrice: '$53',
+    previousPrice: '',
+    badge: '',
+    category: ['men'],
+  },
+  {
+    title: 'Cute sports top',
+    description: 'Light and comforable top. Real convenient during excercise',
+    image: 'v1537361998/samples/girl-running-top.jpg',
+    discountPrice: '$32',
+    previousPrice: '',
+    badge: '',
+    category: ['women'],
+  },
+  {
+    title: 'The T-shirt',
+    description:
+      "It is called the T-shirt because it beats all other T-shrits. It stands out in it's own way",
+    image: 'v1537361998/samples/pants.jpg',
+    discountPrice: '$86',
+    previousPrice: '$110',
+    badge: 'sale',
+    category: ['men', 'sale'],
+  },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "The best coat and wait jacket money can buy. Hand made with long lasting fibre",
+  //   "image": "",
+  //   "discountPrice": "$12",
+  //   "previousPrice": "$16",
+  //   "badge": "new",
+  //   "category": "new"
+  // },
+  // {
+  //   "title": "Non coating waist jacket",
+  //   "description": "This beautiful gown is for walking round the house",
+  //   "image": "",
+  //   "discountPrice": "$30",
+  //   "previousPrice": "$33",
+  //   "badge": "new",
+  //   "category": ["women", "new"]
+  // },
+  // {
+  //   "title": "Khaki shorts",
+  //   "description": "Get the best khaki shorts at a highly discounted rate",
+  //   "image": "",
+  //   "discountPrice": "$20",
+  //   "previousPrice": "$35",
+  //   "badge": "sale",
+  //   "category": ["sale", "men"]
+  // }
+];
