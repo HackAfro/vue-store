1ceb0cc883be923c05a961a5e9adc534da5210f0
Richard Afro <richyafro@hotmail.com>
2018-09-21 09:48:35 +0100

    Update products list

--- products.js
+++ products.js
@@ -1,137 +1,221 @@
 module.exports = [
   {
-    title: 'Non coating waist jacket',
+    title: "Fine women's scarf",
+    url: 'https://shop.lululemon.com/p/women-scarves-gloves/Vinyasa-Scarf-II/_/prod4240002',
+    thumbnails: [
+      'v1537469550/vue-store/women-fine-scarf-thumb-1.jpg',
+      'v1537469559/vue-store/women-fine-scarf-thumb-3.jpg',
+      'v1537469583/vue-store/women-fine-scarf.jpg'
+    ],
     description:
       'The best coat and wait jacket money can buy. Hand made with long lasting fibre',
-    image: 'v1530025449/samples/bike.jpg',
+    image: 'v1537469550/vue-store/women-fine-scarf-thumb-1.jpg',
     discountPrice: '$12',
     previousPrice: '$16',
     badge: 'sale',
     category: ['women', 'sale'],
   },
   {
-    title: 'Best jogging shoes',
+    title: 'Unique pattern shirt',
+    url: 'https://www.myer.com.au/shop/mystore/offers/men-739052-1/bitalli-fern-print-shirt-617270950',
+    thumbnails: [
+      'v1537469572/vue-store/men-pattern-shirt-thumb-1.jpg',
+      'v1537469579/vue-store/men-pattern-shirt-thumb-2.jpg',
+      'v1537469569/vue-store/men-pattern-shirt-thumb-3.jpg'
+    ],
     description:
       'This is the best jogging shoes known to man. Feels sweet on your foot',
-    image: 'v1530025448/samples/ecommerce/shoes.png',
+    image: 'v1537469585/vue-store/men-pattern-shirt.jpg',
     discountPrice: '$60',
     previousPrice: '',
     badge: 'new',
     category: ['new'],
   },
   {
-    title: 'Old school leather bag',
+    title: 'Beautiful lightweight tank',
+    url: 'https://shop.lululemon.com/p/women-tanks/Love-Tank-Pleated/_/prod8980076?color=34432',
+    thumbnails: [
+      'v1537469539/vue-store/women-tank-thumb-1.jpg',
+      'v1537469539/vue-store/women-tank-thumb-2.jpg',
+      'v1537469550/vue-store/women-tank-thumb-3.jpg'
+    ],
     description:
       'The best coat and wait jacket money can buy. Hand made with long lasting fibre',
-    image: 'v1530025457/samples/ecommerce/leather-bag-gray.jpg',
+    image: 'v1537469539/vue-store/women-tank.jpg',
     discountPrice: '$240',
     previousPrice: '$320',
     badge: 'sale',
     category: ['men', 'sale', 'bag'],
   },
   {
-    title: 'Blue winter jacket',
+    title: 'Blue skinny jean',
+    url: 'https://www.myer.com.au/shop/mystore/offers/men-739052-1/zack-2-skinny-jean-614907370',
+    thumbnails: [
+      'v1537469578/vue-store/men-blue-jeans-thumb-1.jpg',
+      'v1537469577/vue-store/men-blue-jeans-thumb-3.jpg',
+      'v1537469575/vue-store/men-blue-jeans-thumb-2.jpg'
+    ],
     description: 'Keeps the cold away, lets the kid enjoy the snow',
-    image: 'v1530025450/samples/people/boy-snow-hoodie.jpg',
+    image: 'v1537469581/vue-store/men-blue-jeans.jpg',
     discountPrice: '$60',
     previousPrice: '',
     badge: '',
     category: ['men'],
   },
   {
-    title: 'Exotic print t-shirt',
+    title: 'Fly lightweight shorts',
+    url: 'https://shop.lululemon.com/p/women-shorts/On-The-Fly-Short/_/prod8430791?color=28603',
+    thumbnails: [
+      'v1537469562/vue-store/women-fly-shorts.jpg',
+      'v1537469544/vue-store/women-fly-shorts-thumb-2.jpg',
+      'v1537469543/vue-store/women-fly-shorts-thumb-3.jpg'
+    ],
     description:
       "Exotic long lasting T-shirt. Lasts longer than you'll expect. No lies spoken",
-    image: 'v1531899989/samples/attendee_avatar.jpg',
+    image: 'v1537469546/vue-store/women-fly-shorts-thumb-1.jpg',
     discountPrice: '$16',
     previousPrice: '',
     badge: '',
     category: ['men', 'shirt'],
   },
   {
-    title: 'The assorted collection',
+    title: 'Swiftly Tech Short Sleeve Crew',
+    url: 'https://shop.lululemon.com/p/tops-short-sleeve/Run-Swiftly-Tech-Short-Sleeve-Crew/_/prod4500151?color=34327',
+    thumbnails: [
+      'v1537469539/vue-store/women-shirt-thumb-1.jpg',
+      'v1537469541/vue-store/women-shirt-thumb-2.jpg',
+      'v1537469544/vue-store/women-shirt-thumb-3.jpg'
+    ],
     description:
       'A collection of our best products yet. At a highly discounted price.',
-    image: 'v1530025458/samples/ecommerce/accessories-bag.jpg',
+    image: 'v1537469552/vue-store/women-shirt-blue.jpg',
     discountPrice: '$300',
     previousPrice: '$415',
     badge: 'sale',
     category: ['sale', 'men', 'shirt'],
   },
+  
   {
-    title: 'Soft blue t-shirt',
-    description:
-      'Plain blue t-shirt that can be worn underneath your everyday jacket. Switch it up',
-    image: 'v1530025447/samples/people/smiling-man.jpg',
-    discountPrice: '$5',
-    previousPrice: '$16',
-    badge: 'new',
-    category: ['new', 'men', 'shirt'],
-  },
-  {
-    title: 'Beautiful self cleaning chef jacket',
+    title: 'MAXI onesie dress',
+    url: 'https://shop.lululemon.com/p/women-maintops/Refresh-Maxi-Dress-II/_/prod2030072?color=1966',
+    thumbnails: [
+      'v1537516087/vue-store/women-maxi-dress-thumb-1.jpg',
+      'v1537516086/vue-store/women-max-dress-thumb-2.jpg'
+    ],
     description:
       "What's better than a self cleaning jacket. Don't beat yourself up for spilling food on it. It knows you're clumsy",
-    image: 'v1530025442/samples/people/kitchen-bar.jpg',
+    image: 'v1537516086/vue-store/women-maxi-dress.jpg',
     discountPrice: '$46',
     previousPrice: '',
     badge: '',
     category: ['men'],
   },
   {
-    title: 'Brown leather wrist watch',
+    title: 'Comfy jogging shoes',
+    url: 'https://kswiss.com/collections/mens/products/05881-039-m',
+    thumbnails: [
+      'v1537469560/vue-store/men-swiss-shoe-thumb-1.png',
+      'v1537469558/vue-store/men-swiss-shoe-thumb-2.png',
+      'v1537469560/vue-store/men-swiss-shoe-thumb-3.png'
+    ],
+    description:
+      'Plain blue t-shirt that can be worn underneath your everyday jacket. Switch it up',
+    image: 'v1537469566/vue-store/men-swiss-shoe.png',
+    discountPrice: '$5',
+    previousPrice: '$16',
+    badge: 'new',
+    category: ['new', 'men', 'shirt'],
+  },
+  {
+    title: 'Day Tripper  dress',
+    url: 'https://shop.lululemon.com/p/womens-gifts-for-to-from/Day-Tripper-Dress/_/prod8900586?color=11547',
+    thumbnails: [
+      'v1537516087/vue-store/women-tripper-dress-thumb-1.jpg',
+      'v1537516084/vue-store/women-tripper-dress-thumb-2.jpg',
+      'v1537516085/vue-store/women-tripper-dress-thumb-3.jpg'
+    ],
     description:
       'The kind you hand over to your grandkids when they start being nice to you',
-    image: 'v1530025440/samples/ecommerce/analog-classic.jpg',
+    image: 'v1537516087/vue-store/women-tripper-dress.jpg',
     discountPrice: '$120',
     previousPrice: '$156',
     badge: 'sale',
     category: ['sale', 'men'],
   },
   {
-    title: 'Comfortable flannel shrit',
+    title: 'Sojourn Hoodie 20th Anniversary',
+    url: 'https://shop.lululemon.com/p/mens-jackets-and-hoodies-hoodies/Sojourn-Hoodie-20Th-Anniversary/_/prod9090016?color=28948',
+    thumbnails: [
+      'v1537516084/vue-store/men-20y-hoodie-thumb-1.jpg',
+      'v1537516084/vue-store/men-20y-hoodie-thumb-2.jpg',
+      'v1537516084/vue-store/men-20y-hoodie-thumb-3.jpg'
+    ],
     description:
       'Real comfy flannel shirt with high quality cotton. Available in a set of bright colors',
-    image: 'v1537361999/samples/flannel-shirt.jpg',
+    image: 'v1537516084/vue-store/men-20y-hoodie.jpg',
     discountPrice: '$120',
     previousPrice: '',
     badge: 'new',
     category: ['new', 'men', 'shirt'],
   },
+  
   {
-    title: 'Non coating waist jacket',
-    description:
-      'The best coat and wait jacket money can buy. Hand made with long lasting fibre',
-    image: 'v1537361998/samples/man-on-balcony.jpg',
-    discountPrice: '$120',
-    previousPrice: '$210',
-    badge: 'sale',
-    category: ['sale', 'men'],
-  },
-  {
-    title: 'Grey thick hoodie',
+    title: 'Wunder Under crop (HiRise)',
+    url: 'https://shop.lululemon.com/p/women-crops/Wunder-Under-Crop-Hi-Rise-Luon/_/prod290003?color=28510',
+    thumbnails: [
+      'v1537516085/vue-store/women-wunder-crop-thumb-1.jpg',
+      'v1537516086/vue-store/women-wunder-crop-thumb-2.jpg',
+      'v1537516087/vue-store/women-wunder-crop-thumb-3.jpg'
+    ],
     description:
       'Attack the cold with this comfy hoodie that keeps you warm during that cold evening run',
-    image: 'v1537361998/samples/hoodie.jpg',
+    image: 'v1537516085/vue-store/women-wunder-crop.jpg',
     discountPrice: '$53',
     previousPrice: '',
     badge: '',
     category: ['men', 'shirt'],
   },
   {
-    title: 'Cute sports top',
+    title: 'Dry Sense Hoodie',
+    url: 'https://shop.lululemon.com/p/mens-jackets-and-hoodies-hoodies/Drysense-Hoodie/_/prod9090026?color=33121',
+    thumbnails: [
+      'v1537516084/vue-store/men-dry-sense-hoodie-thumb-1.jpg',
+      'v1537516085/vue-store/men-dry-sense-hoodie-thumb-2.jpg',
+      'v1537516085/vue-store/men-dry-sense-hoodie-thumb-3.jpg'
+    ],
+    description:
+      'The best coat and wait jacket money can buy. Hand made with long lasting fibre',
+    image: 'v1537516084/vue-store/men-dry-sense-hoodie.jpg',
+    discountPrice: '$120',
+    previousPrice: '$210',
+    badge: 'sale',
+    category: ['sale', 'men'],
+  },
+  {
+    title: 'Final Lap Crop',
+    url: 'https://shop.lululemon.com/p/women-crops/Final-Lap-Crop/_/prod8555484?color=34270',
+    thumbnails: [
+      'v1537516086/vue-store/women-lap-crop-thumb-1.jpg',
+      'v1537516086/vue-store/women-lap-crop-thumb-2.jpg',
+      'v1537516086/vue-store/women-lap-crop-thumb-3.jpg'
+    ],
     description: 'Light and comforable top. Real convenient during excercise',
-    image: 'v1537361998/samples/girl-running-top.jpg',
+    image: 'v1537516085/vue-store/women-lap-crop.jpg',
     discountPrice: '$32',
     previousPrice: '',
     badge: '',
     category: ['women', 'shirt'],
   },
   {
-    title: 'The T-shirt',
+    title: 'Comfortable Knit wear sweaters',
+    url: 'https://www.myer.com.au/shop/mystore/men/mens-clothing/knitwear-sweaters/superdry-sweat-shirt-store-tri-hood',
+    thumbnails: [
+      'v1537516086/vue-store/men-dry-sweat-shirt-thumb-1.jpg',
+      'v1537516086/vue-store/men-dry-sweat-shirt-thumb-2.jpg',
+    ],
     description:
       "It is called the T-shirt because it beats all other T-shrits. It stands out in it's own way",
-    image: 'v1537361998/samples/pants.jpg',
+    image: 'v1537516085/vue-store/men-dry-sweat-shirt.jpg',
     discountPrice: '$86',
     previousPrice: '$110',
     badge: 'sale',
